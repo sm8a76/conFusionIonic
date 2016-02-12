b707eaac044d104f364f20765a69e935e7aa8f42
Sergio Mendoza <sm8a76@gmail.com>
2016-02-12 17:59:15 -0600

    Added resolve object to app/home state

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -239,32 +239,15 @@ angular.module('conFusion.controllers', [])
 
         // implement the IndexController and About Controller here
 
-.controller('IndexController', ['$scope', 'menuFactory', 'promotionFactory', 'corporateFactory', 'baseURL', function($scope, menuFactory, promotionFactory, corporateFactory, baseURL) {
+.controller('IndexController', ['$scope', 'dish', 'leader', 'promotion', 'baseURL', function($scope, dish, leader, promotion, baseURL) {
                                         
                         $scope.baseURL = baseURL;
-                        $scope.leader = corporateFactory.get({
-                            id: 3
-                        });    
-    
                         $scope.showDish = false;
                         $scope.message="Loading ...";
-    
-                        $scope.dish = menuFactory.get({
-                                id: 0
-                            })
-                            .$promise.then(
-                                function (response) {
-                                    $scope.dish = response;
-                                    $scope.showDish = true;
-                                },
-                                function (response) {
-                                    $scope.message = "Error: " + response.status + " " + response.statusText;
-                                }
-                            );
 
-                        $scope.promotion = promotionFactory.get({
-                            id: 0
-                        });    
+                        $scope.leader = leader;
+                        $scope.dish = dish;
+                        $scope.promotion = promotion;
     
                     }])
 
