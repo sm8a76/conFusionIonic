b7e910f5b083b7cfa665edd61f70a14f60439108
Sergio Mendoza <sm8a76@gmail.com>
2016-02-16 19:43:07 -0600

    Customizing and Controlling the Splash Screen

--- www/js/services.js
+++ www/js/services.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('conFusion.services', ['ngResource'])
-        .constant("baseURL","http://localhost:3000/")
+        .constant("baseURL","http://10.62.101.12:3000/")
         .factory('menuFactory', ['$resource', 'baseURL', function($resource,baseURL) {
     
             return $resource(baseURL + "dishes/:id", null, {
